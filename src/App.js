ad1da76fa870d889c6353699f937ea8a779e9102
Shailendrakori <113526148+Shailendrakori@users.noreply.github.com>
2022-12-23 18:30:52 +0530

    comkladhn

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 
-          dffdd
+          Edit <code>src/App.js</code> hellon coder
         </p>
         <a
           className="App-link"
